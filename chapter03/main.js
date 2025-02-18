bb1e3eabf3c82facdd782a123b3a99dcf924c64f
BrandonVasquez23 <23.bvasquez@gmail.com>
2025-02-17 20:50:27 -0600

    went up to pg 66 of chapter 3 pdf

--- chapter03/main.js
+++ chapter03/main.js
@@ -1,4 +1,16 @@
 console.log("Hello from main.js");
 
 let targetElement = document.querySelector("h2");
+console.log(targetElement);
+
+targetElement.addEventListener("mouseover", funtion() {
+    console.log(this.innerText);
+}
+);
+this.innerText = "TIME";
+
+targetElement.addEventListener("mouseout", function(){
+    this.innerText = "IKEA";
+}
+);
 <!--left off on pg 64 of the pdf -->
