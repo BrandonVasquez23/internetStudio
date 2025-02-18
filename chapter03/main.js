336884a8ac4479b383e19353a8c78c56220acdc0
BrandonVasquez23 <23.bvasquez@gmail.com>
2025-02-17 20:59:09 -0600

    finished chapter03 pdf

--- chapter03/main.js
+++ chapter03/main.js
@@ -3,14 +3,16 @@ console.log("Hello from main.js");
 let targetElement = document.querySelector("h2");
 console.log(targetElement);
 
-targetElement.addEventListener("mouseover", funtion() {
+targetElement.addEventListener("mouseover", function(){
     console.log(this.innerText);
+    this.style = "cursor: wait";
 }
 );
 this.innerText = "TIME";
 
 targetElement.addEventListener("mouseout", function(){
     this.innerText = "IKEA";
+    this.style = "cursor: pointer";
 }
 );
 <!--left off on pg 64 of the pdf -->
